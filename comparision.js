126da8958ad4cbec609a7961ba07db9c07b0c8b1
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2025-06-14 10:28:15 +0530

    comparision

---
+++ comparision.js
@@ -0,0 +1,20 @@
+console.log(2==2);
+console.log(2===2)
+
+let fistnum=2;
+let secondnum="2"
+console.log(fistnum==secondnum); // value comparision and coercion convertion
+console.log(fistnum===secondnum); // data type comparision no type coercion
+
+
+// comparison will perform coercion
+console.log("\n null checking");
+
+let a=null
+let b=0
+console.log(a==b);
+console.log(a===b);
+console.log(a>=b); // comparision operator converts null to a number
+
+
+
