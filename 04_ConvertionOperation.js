f8438acb31fd80695856d75e24e73112293c8e8b
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2025-06-14 09:52:20 +0530

    convertion of data

---
+++ 04_ConvertionOperation.js
@@ -0,0 +1,34 @@
+console.log("Checking the type");
+let score=35;
+console.log(typeof(score));
+console.log(typeof score);
+
+console.log("\nChecking the type Number");
+let data="35"  //can be converted to Number
+console.log(typeof data);
+data=Number(data)
+console.log("data type of data is : ",typeof data);
+
+console.log("\nChecking the type Number and string");
+let check="35sf" // cannot be converted to int (NaN)
+check=Number(check)
+console.log(typeof data);
+console.log(check);
+
+console.log("\n\nChecking the type null");
+let temp=null
+console.log(temp);
+console.log(Number(temp)); // when null converted to Number it will become 0
+
+console.log("\n\nChecking the type of boolean");
+let isLoggedIn="r"; //other than 0 and "" Boolean will return true
+let checkLoggedIN=Boolean(isLoggedIn)
+console.log(checkLoggedIN);
+
+
+console.log("\n\nConverting Number to string");
+let someNumber=33;
+let StringNumber=String(someNumber)
+console.log(typeof StringNumber)
+
+
